7527f0dd813cfc1fd672b1dac98704d5fd84cd22
touhou09 <touhou3178@gmail.com>
2024-02-02 02:32:12 +0000

    tweet post page making

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,13 @@
+import styled from "styled-components";
 import { auth } from "../firebase"
+import PostTweetForm from "../component/post-tweet-form";
+
+const Wrapper = styled.div``;
 
 export default function Home(){
-    const logOut = () =>{
-        auth.signOut();
-    }
     return (
-    <h1>
-        <button onClick={logOut}>LogOut</button>
-    </h1>
+        <Wrapper>
+            <PostTweetForm />
+        </Wrapper>
     )
 }
